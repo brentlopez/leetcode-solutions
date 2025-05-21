e7ad314b1daa29ac2a6647023a030534f847f9bb
Brent Lopez <brent@brentlopez.dev>
2025-05-20 20:11:06 -0700

    Complete Leetcode 1614

---
+++ meta/1614-maximum-nesting-depth-of-the-parentheses.cs
@@ -0,0 +1,32 @@
+// 1614. Maximum Nesting Depth of the Parentheses
+/*
+ * Completion Time:
+ *  Attempt 1: 4 minutes
+ *
+*/
+
+
+public class Solution
+{
+    public int MaxDepth(string s)
+    {
+        var maxDepth = 0;
+        var currentDepth = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i].Equals('('))
+            {
+                currentDepth++;
+            }
+
+            if (s[i].Equals(')'))
+            {
+                currentDepth--;
+            }
+
+            maxDepth = Math.Max(maxDepth, currentDepth);
+        }
+
+        return maxDepth;
+    }
+}
